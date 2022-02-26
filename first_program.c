6ad42af79213e1a7d375ecb1b778d997ddd11e0f
Himanshu D Hatwar <hatwarhimanshu1412@gmail.com>
2022-02-26 22:34:15 +0530

    First Program

--- first_program.c
+++ first_program.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    printf("This is my First C Program");       // Simple print message
+    printf("This is my first C Program");       // Simple print message
     return 0;
 }
