c861096d12fae97019ce0be645babc69b05aff02
Himanshu D Hatwar <hatwarhimanshu1412@gmail.com>
2022-02-27 20:23:40 +0530

    Grades Calculating Program

---
+++ Grades_of_student.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int marks;                                                              // Initializing the variables
+    
+    printf("Enter your marks\n");                                           // Print message
+    scanf("%d",&marks);                                                     // Get input of marks from user
+    
+    if (marks>85 && marks<=100)
+    {
+        printf("Congratulations !! You secured A+ Grade......");            // if block
+    }
+    
+    else if (marks>60 && marks<=85)
+    {
+        printf("You secured A Grade......");                                // else-if block
+    }
+    
+    else if (marks>40 && marks<=60)
+    {
+        printf("You secured A Grade......");                                // else-if block                         
+    }
+    
+    else if (marks>30 && marks<=40)
+    {
+        printf("You secured C Grade......");                                // else-if block
+    }
+    
+    else
+    {
+        printf("You have Failed......");                                    // else block
+    }
+    
+    return 0;
+}
